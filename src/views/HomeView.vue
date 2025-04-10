aec054143a82519b1eef6560afeff94230fd8f45
Nikita <getingjagare@gmail.com>
2025-04-10 14:25:52 +0200

    posts search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@ import type { Post } from '@/types/post'
 const perPage = ref<number>(10)
 const page = ref<number>(1)
 const posts = ref<Post[]>([])
+const search = ref<string>('')
 
 onMounted(async () => {
   const { data }: { data: Post[] } = await axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -19,10 +20,26 @@ const goToNextPage = () => {
   }
 }
 
-const visiblePosts: Post[] = computed(() => posts.value.slice(0, page.value * perPage.value))
+const filteredPosts = computed(() => {
+  if (!search.value) {
+    return posts.value
+  }
+
+  return posts.value.filter((post) => post.title.toLowerCase().includes(search.value.toLowerCase()))
+})
+
+const visiblePosts: Post[] = computed(() =>
+  filteredPosts.value.slice(0, page.value * perPage.value),
+)
 </script>
 
 <template>
+  <div class="mb-3">
+    <h1 class="text-left">Posts</h1>
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Search posts..." v-model="search" />
+    </div>
+  </div>
   <div class="row g-2">
     <div class="col-12 col-sm-6 col-lg-4" v-for="post in visiblePosts" :key="post.id">
       <div class="card mt-0 h-100 w-100">
@@ -34,7 +51,7 @@ const visiblePosts: Post[] = computed(() => posts.value.slice(0, page.value * pe
       </div>
     </div>
   </div>
-  <div class="mt-3 text-center" v-if="visiblePosts.length < posts.length">
+  <div class="mt-3 text-center" v-if="visiblePosts.length < filteredPosts.length">
     <button type="button" class="btn btn-light" @click="goToNextPage">Show More</button>
   </div>
 </template>
